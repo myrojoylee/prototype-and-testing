e990354cf3381ecf4332bc283c6b18189de07cbc
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-04-19 00:01:11 -0400

    added starter files

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+  $("#speed").selectmenu();
+
+  $("#files").selectmenu();
+
+  $("#number").selectmenu().selectmenu("menuWidget").addClass("overflow");
+
+  $("#salutation").selectmenu();
+});
