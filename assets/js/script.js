d7c17e9cdf209cefedd14a440b9626122a857140
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-04-19 22:47:00 -0400

    added code to test out API and date input field

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,33 @@
+let allData, zipCode;
+const submitBtn = document.querySelector("#submit");
+const userZipCode = document.querySelector("#search-zipcode");
+
+submitBtn.addEventListener("click", function () {
+  console.log(Number(userZipCode.value));
+  zipCode = Number(userZipCode.value);
+  fetchMasterData(zipCode);
+});
+
+function fetchMasterData(zipCode) {
+  let postalURL = `https://api.openbrewerydb.org/v1/breweries?by_postal=${zipCode}`;
+
+  fetch(postalURL)
+    .then((response) => response.json())
+    .then((allData) => console.log(allData));
+}
+
 $(function () {
-  $("#speed").selectmenu();
-
-  $("#files").selectmenu();
-
-  $("#number").selectmenu().selectmenu("menuWidget").addClass("overflow");
-
+  $("#size").selectmenu();
   $("#salutation").selectmenu();
 });
+
+/**
+ * datepicker function
+ */
+$(function () {
+  $("#datepicker").datepicker();
+  const value = $("#datepicker").val();
+  console.log(value);
+});
+
+// userZipCode = document.querySelector("#search-zipcode").value;
